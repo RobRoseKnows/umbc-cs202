7437ef5af04a0960195dff38dddabe74d4f3f927
Robert Rose <bertra97@gmail.com>
2016-02-04 21:50:01 -0500

    First lab, simple program.

---
+++ Projects/Proj0/proj0.cpp
@@ -0,0 +1,15 @@
+/* File:    proj0.cpp
+ * Name:    Robert Rose
+ * Date:    2/4/16
+ * Section: 3
+ * E-mail:  robrose2@umbc.edu
+ */
+
+#include <iostream> 
+using namespace std;
+
+int main() {
+  cout << "Hello, world!" << endl;
+
+  return 0;
+}
